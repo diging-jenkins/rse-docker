49cf23683b65f2176c06345616c513b257ae44f8
DigInG Jenkins <diging@asu.edu>
2023-03-13 10:26:19 -0400

    Added another message.

--- convert.py
+++ convert.py
@@ -1,3 +1,4 @@
 celsius = float(input("Please, enter temperature in Celsius: "))
 fahrenheit = (celsius * 1.8) + 32 
 print("{0} Celsius is {1} Fahrenheit.".format(celsius, fahrenheit)) 
+print("This was easy!")
